43d0640644163550523bcd0f25df44e46d341706
adwaittawde16-gif <adwaittawde16@gmail.com>
2026-01-14 19:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (50.82%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.c
@@ -0,0 +1,18 @@
+
+    char findTheDifference(char* s, char* t) {
+    char result = 0;
+
+    while (*s) {
+        result ^= *s;
+        s++;
+    }
+
+    while (*t) {
+        result ^= *t;
+        t++;
+    }
+
+    return result;
+}
+
+    
