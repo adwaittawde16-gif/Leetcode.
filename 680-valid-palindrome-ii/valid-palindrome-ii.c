ba9b2497fe34c2ca1662e8098430c9a2ae4f5fd2
adwaittawde16-gif <adwaittawde16@gmail.com>
2026-01-17 18:49:08 +0530

    Time: 3 ms (42.54%) | Memory: 11.4 MB (54.39%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool isPalindromeRange(char* s, int left, int right) {
+    while (left < right) {
+        if (s[left] != s[right])
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+bool validPalindrome(char* s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            // Try skipping either left or right character
+            return isPalindromeRange(s, left + 1, right) ||
+                   isPalindromeRange(s, left, right - 1);
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
