b68bec200e9800d10c34f4b68535604d313b5d5b
adwaittawde16-gif <adwaittawde16@gmail.com>
2026-03-21 21:09:32 +0530

    Time: 2 ms (67.49%) | Memory: 85.3 MB (54.76%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int inc = 1;
-        int dec = 1;
+        int inc = 1,dec = 1;
         for (int i = 0 ; i < nums.length-1 ; i++)
         {
             if(nums[i]>nums[i+1])
