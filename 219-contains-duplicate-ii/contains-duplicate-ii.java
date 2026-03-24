e2f408ed49f5e41e8764cd91ae7004182050a85e
adwaittawde16-gif <adwaittawde16@gmail.com>
2026-03-24 23:02:48 +0530

    Time: 23 ms (81.07%) | Memory: 90.8 MB (22.62%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!map.containsKey(nums[i]))
+            {
+                map.put(nums[i],i);
+            }
+            else
+            {
+                int sub=Math.abs(map.get(nums[i])-i);
+                if(sub<=k)
+                {
+                    return true;
+                }
+                else
+                {
+                    map.put(nums[i],i);
+                }
+            }
+        }
+        return false;
+    }
+}
