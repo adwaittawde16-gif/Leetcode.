bb2db6db03b7a9ac31019ecbc740c114fa4f46cd
adwaittawde16-gif <adwaittawde16@gmail.com>
2026-03-25 22:59:04 +0530

    Time: 1 ms (12.65%) | Memory: 42.9 MB (34.46%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        if(s==null || s.length()==0)
+        {
+            return 0;
+        }
+        int left=0,right=0,count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch=='R')
+            {
+                right++;
+            }
+            else if(ch =='L')
+            {
+                left++;
+            }
+            if(right==left)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
