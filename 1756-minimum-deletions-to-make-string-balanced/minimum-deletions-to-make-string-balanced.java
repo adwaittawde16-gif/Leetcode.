6fd19fae6a6eb87ae3e5d66b5438b083727f49a6
adwaittawde16-gif <adwaittawde16@gmail.com>
2026-04-01 23:05:29 +0530

    Time: 135 ms (5.02%) | Memory: 47.7 MB (62.70%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        Stack<Character>stack=new Stack<>();
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(!stack.isEmpty() && stack.peek()=='b' && s.charAt(i)=='a')
+            {
+                stack.pop();
+                count++;
+            }
+            else
+            {
+                stack.add(s.charAt(i));
+            }
+        }
+        return count;
+    }
+}
